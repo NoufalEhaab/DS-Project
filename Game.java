677afeff26c5e03db1577f9ef64f7ac528e7663b
NoufalEhaab <104385351+NoufalEhaab@users.noreply.github.com>
2022-12-04 15:49:40 +0500

    Lil customization, more to do

--- Game.java
+++ Game.java
@@ -9,14 +9,23 @@ public class Game {
     static int choice = 0;
     public Game() {
         Scanner input = new Scanner(System.in);
+        System.out.println(Color.RED);
         System.out.print("Enter the number of players : ");
+        System.out.print(Color.RESET);
         int NoOfplayer = input.nextInt();
         for (int i = 0; i < NoOfplayer; i++) {
+            System.out.print(Color.CYAN);
             System.out.print("Enter the name of  Player#" + (i + 1) + " : ");
+            System.out.print(Color.RESET);
             Player a = new Player(input.next());
             players.add(a);
         }
-        System.out.println("Select Difficulty :\n1.Easy\n2.Medium\n3.Hard");
+        System.out.println(Color.RED_UNDERLINED);
+        System.out.println("Select Difficulty :");
+        System.out.print(Color.RESET);
+        System.out.println(Color.CYAN);
+        System.out.println("1.Easy\n2.Medium\n3.Hard");
+        System.out.println(Color.RESET);
         choice = input.nextInt();
         if (choice == 1) {
             maze = new HashMap<>();
@@ -122,13 +131,22 @@ public class Game {
         while (n < 5) {
             // n = x-11;
             for (int i = x; i > x - 10; i--) {
-                System.out.print(a.get(i) + " ");
+                if (a.get(i) == -1) {
+                    System.out.print(Color.YELLOW_BOLD_BRIGHT);
+                    System.out.print((i) + " ");
+                    System.out.print(Color.RESET);
+                }else{
+                    System.out.print(a.get(i) + " ");
+                }
             }
             x = x - 19;
             System.out.println();
             for (int i = x; i < x + 10; i++) {
-                System.out.print(a.get(i) + " ");
-
+                if (a.get(i) == -1) {
+                    System.out.print((i) + " ");
+                }else{
+                    System.out.print(a.get(i) + " ");
+                }
             }
             System.out.println();
             x--;
@@ -209,9 +227,11 @@ public class Game {
     }
 
     public void showposition(ArrayList<Player> player) {
+        System.out.print(Color.CYAN);
         for (Player p : player) {
             System.out.println("Player " + p.name + " position = " + p.step);
         }
+        System.out.print(Color.RESET);
     }
 
     public void movesnake(Player p) {
@@ -266,22 +286,122 @@ public class Game {
         System.out.println("Minimum number of steps in which Player "+player.name + " can finish game = " + player.movesrequire );
         }
     }
-
+    enum Color {
+        //Color end string, color reset
+        RESET("\033[0m"),
+    
+        // Regular Colors. Normal color, no bold, background color etc.
+        BLACK("\033[0;30m"),    // BLACK
+        RED("\033[0;31m"),      // RED
+        GREEN("\033[0;32m"),    // GREEN
+        YELLOW("\033[0;33m"),   // YELLOW
+        BLUE("\033[0;34m"),     // BLUE
+        MAGENTA("\033[0;35m"),  // MAGENTA
+        CYAN("\033[0;36m"),     // CYAN
+        WHITE("\033[0;37m"),    // WHITE
+    
+        // Bold
+        BLACK_BOLD("\033[1;30m"),   // BLACK
+        RED_BOLD("\033[1;31m"),     // RED
+        GREEN_BOLD("\033[1;32m"),   // GREEN
+        YELLOW_BOLD("\033[1;33m"),  // YELLOW
+        BLUE_BOLD("\033[1;34m"),    // BLUE
+        MAGENTA_BOLD("\033[1;35m"), // MAGENTA
+        CYAN_BOLD("\033[1;36m"),    // CYAN
+        WHITE_BOLD("\033[1;37m"),   // WHITE
+    
+        // Underline
+        BLACK_UNDERLINED("\033[4;30m"),     // BLACK
+        RED_UNDERLINED("\033[4;31m"),       // RED
+        GREEN_UNDERLINED("\033[4;32m"),     // GREEN
+        YELLOW_UNDERLINED("\033[4;33m"),    // YELLOW
+        BLUE_UNDERLINED("\033[4;34m"),      // BLUE
+        MAGENTA_UNDERLINED("\033[4;35m"),   // MAGENTA
+        CYAN_UNDERLINED("\033[4;36m"),      // CYAN
+        WHITE_UNDERLINED("\033[4;37m"),     // WHITE
+    
+        // Background
+        BLACK_BACKGROUND("\033[40m"),   // BLACK
+        RED_BACKGROUND("\033[41m"),     // RED
+        GREEN_BACKGROUND("\033[42m"),   // GREEN
+        YELLOW_BACKGROUND("\033[43m"),  // YELLOW
+        BLUE_BACKGROUND("\033[44m"),    // BLUE
+        MAGENTA_BACKGROUND("\033[45m"), // MAGENTA
+        CYAN_BACKGROUND("\033[46m"),    // CYAN
+        WHITE_BACKGROUND("\033[47m"),   // WHITE
+    
+        // High Intensity
+        BLACK_BRIGHT("\033[0;90m"),     // BLACK
+        RED_BRIGHT("\033[0;91m"),       // RED
+        GREEN_BRIGHT("\033[0;92m"),     // GREEN
+        YELLOW_BRIGHT("\033[0;93m"),    // YELLOW
+        BLUE_BRIGHT("\033[0;94m"),      // BLUE
+        MAGENTA_BRIGHT("\033[0;95m"),   // MAGENTA
+        CYAN_BRIGHT("\033[0;96m"),      // CYAN
+        WHITE_BRIGHT("\033[0;97m"),     // WHITE
+    
+        // Bold High Intensity
+        BLACK_BOLD_BRIGHT("\033[1;90m"),    // BLACK
+        RED_BOLD_BRIGHT("\033[1;91m"),      // RED
+        GREEN_BOLD_BRIGHT("\033[1;92m"),    // GREEN
+        YELLOW_BOLD_BRIGHT("\033[1;93m"),   // YELLOW
+        BLUE_BOLD_BRIGHT("\033[1;94m"),     // BLUE
+        MAGENTA_BOLD_BRIGHT("\033[1;95m"),  // MAGENTA
+        CYAN_BOLD_BRIGHT("\033[1;96m"),     // CYAN
+        WHITE_BOLD_BRIGHT("\033[1;97m"),    // WHITE
+    
+        // High Intensity backgrounds
+        BLACK_BACKGROUND_BRIGHT("\033[0;100m"),     // BLACK
+        RED_BACKGROUND_BRIGHT("\033[0;101m"),       // RED
+        GREEN_BACKGROUND_BRIGHT("\033[0;102m"),     // GREEN
+        YELLOW_BACKGROUND_BRIGHT("\033[0;103m"),    // YELLOW
+        BLUE_BACKGROUND_BRIGHT("\033[0;104m"),      // BLUE
+        MAGENTA_BACKGROUND_BRIGHT("\033[0;105m"),   // MAGENTA
+        CYAN_BACKGROUND_BRIGHT("\033[0;106m"),      // CYAN
+        WHITE_BACKGROUND_BRIGHT("\033[0;107m");     // WHITE
+    
+        private final String code;
+    
+        Color(String code) {
+            this.code = code;
+        }
+    
+        @Override
+        public String toString() {
+            return code;
+        }
+    }
     public static void main(String[] args) {
-        Game g = new Game();
-        printmaze(maze);
+        System.out.println(Color.RED);
+        System.out.println(Color.GREEN_BACKGROUND);
+        System.out.println("  _________              __                      _____              .___ .____                .___  .___                           ");
+          System.out.println( "/   _____/ ____ _____  |  | __ ____   ______   /  _  \\   ____    __| _/ |    |   _____     __| _/__| _/___________             ");
+          System.out.println("\\_____  \\ /    \\__  \\ |  |/ // __ \\ /  ___/  /  /_\\  \\ /    \\  / __ |  |    |   \\__  \\   / __ |/ __ |/ __ \\_  __ \\   ") ;
+          System.out.println("/        \\   |  \\/ __ \\|    <\\  ___/ \\___ \\  /    |    \\   |  \\/ /_/ |  |    |___ / __ \\_/ /_/ / /_/ \\  ___/|  | \\/   ");
+          System.out.println("/_______  /___|  (____  /__|_ \\___  >____  > \\____|__  /___|  /\\____ |  |_______ (____  /\\____ \\____ |\\___  >__|           ");
+          System.out.println("        \\/     \\/     \\/     \\/    \\/     \\/          \\/     \\/      \\/          \\/    \\/      \\/    \\/    \\/    "  );
+        System.out.println(Color.RESET);
+       
+          Game g = new Game();
+        // printmaze(maze);
         System.out.println();
         while (0 == 0) {
             printmaze(maze);
             g.showposition(g.players);
             for (Player p : g.players) {
+                System.out.print(Color.GREEN_BOLD_BRIGHT);
                 System.out.println("Player " + p.name + " turn : ");
+                System.out.print(Color.RESET);
+                System.out.print(Color.BLUE);
                 System.out.println("Press r to roll dice!");
+                System.out.print(Color.RESET);
                 Scanner sc = new Scanner(System.in);
                 String a = sc.next();
                 if (a.equals("r")) {
                     int num = g.rolldice();
+                    System.out.print(Color.RED_BOLD_BRIGHT);
                     System.out.println("Number = " + num);
+                    System.out.print(Color.RESET);
                     if (choice == 1) {
                     g.step_assign(p, num);
                     }
